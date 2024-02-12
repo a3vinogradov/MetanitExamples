a4d3ff74aaf9d681990867a9135ec5686e59b68d
a3vinogradov <a3vinogradov@gmail.com>
2024-02-12 16:53:53 +0300

    JQuery Глава 7

--- JQuery/CH06/Program.cs
+++ JQuery/CH06/Program.cs
@@ -5,9 +5,11 @@ app.UseStaticFiles();   // добавляем поддержку статических файлов
 //app.Run(async (context) => await context.Response.SendFileAsync("index.html"));
 //app.Run(async (context) => await context.Response.WriteAsync("Hello World"));
 //app.Run(async (context) => await context.Response.WriteAsync("Hello METANIT.COM"));
-app.Map("/time", () => $"time:{DateTime.Now.ToShortTimeString()}");
-app.Map("/", async (context) => await context.Response.SendFileAsync("index.html"));
-app.Map("/about", () => "About Page");
-app.Map("/contact", () => "Contacts Page");
+app.Map("/time", () => $"time:{DateTime.Now.ToLongTimeString()}");
+app.Map("/ajax2-1", (string event1, string date) => $"<h3>{event1}</h3><h5>{date}</h5>");
+//app.Map("/about", () => "About Page");
+//app.Map("/contact", () => "Contacts Page");
 //app.UseWelcomePage();
+//app.Run(async (context) => await context.Response.SendFileAsync("index.html"));
+//app.Run(async (context) => await context.Response.WriteAsync("Hello world!!!"));
 app.Run();
